3e2ff3e689b0d033cee5cc36b9e2231941c61a33
terraputix <terraputix@mailbox.org>
2024-10-30 14:43:17 +0100

    fix write_chunks test

--- src/om/writer.rs
+++ src/om/writer.rs
@@ -686,56 +686,56 @@ mod tests {
         writer.write_data(
             &[2.0, 3.0, 7.0, 8.0],
             &[2, 2],
-            &[0..2, 2..4],
+            &[0..2, 0..2],
             &mut file_handle,
             &mut buffer,
         )?;
         writer.write_data(
             &[4.0, 9.0],
             &[2, 1],
-            &[0..2, 4..5],
+            &[0..2, 0..1],
             &mut file_handle,
             &mut buffer,
         )?;
         writer.write_data(
             &[10.0, 11.0, 15.0, 16.0],
             &[2, 2],
-            &[2..4, 0..2],
+            &[0..2, 0..2],
             &mut file_handle,
             &mut buffer,
         )?;
         writer.write_data(
             &[12.0, 13.0, 17.0, 18.0],
             &[2, 2],
-            &[2..4, 2..4],
+            &[0..2, 0..2],
             &mut file_handle,
             &mut buffer,
         )?;
         writer.write_data(
             &[14.0, 19.0],
             &[2, 1],
-            &[2..4, 4..5],
+            &[0..2, 0..1],
             &mut file_handle,
             &mut buffer,
         )?;
         writer.write_data(
             &[20.0, 21.0],
             &[1, 2],
-            &[4..5, 0..2],
+            &[0..1, 0..2],
             &mut file_handle,
             &mut buffer,
         )?;
         writer.write_data(
             &[22.0, 23.0],
             &[1, 2],
-            &[4..5, 2..4],
+            &[0..1, 0..2],
             &mut file_handle,
             &mut buffer,
         )?;
         writer.write_data(
             &[24.0],
             &[1, 1],
-            &[4..5, 4..5],
+            &[0..1, 0..1],
             &mut file_handle,
             &mut buffer,
         )?;
