69afc5de056b3cde536e26a61caa026c757b4e19
terraputix <terraputix@mailbox.org>
2024-11-28 11:24:56 +0100

    error cleanup

--- src/om/errors.rs
+++ src/om/errors.rs
@@ -5,23 +5,7 @@ pub enum OmFilesRsError {
         errno: i32,
         error: String,
     },
-    CannotCreateFile {
-        filename: String,
-        errno: i32,
-        error: String,
-    },
-    CannotTruncateFile {
-        filename: String,
-        errno: i32,
-        error: String,
-    },
     CannotOpenFileErrno {
-        errno: i32,
-        error: String,
-    },
-    CannotMoveFile {
-        from: String,
-        to: String,
         errno: i32,
         error: String,
     },
@@ -32,23 +16,12 @@ pub enum OmFilesRsError {
     },
     ChunkDimensionIsSmallerThanOverallDim,
     DimensionMustBeLargerThan0,
-    NotAOmFile,
-    InvalidHeaderLength,
-    JSONSerializationError,
     FileExistsAlready {
         filename: String,
-    },
-    PosixFallocateFailed {
-        error: i32,
-    },
-    FtruncateFailed {
-        error: i32,
     },
     InvalidCompressionType,
     InvalidDataType,
     DecoderError(String),
-    TryingToWriteToReadOnlyFile,
-    UnknownVersion(u8),
 }
 
 impl std::fmt::Display for OmFilesRsError {
@@ -65,42 +38,8 @@ impl std::fmt::Display for OmFilesRsError {
                     filename, errno, error
                 )
             }
-            OmFilesRsError::CannotCreateFile {
-                filename,
-                errno,
-                error,
-            } => {
-                write!(
-                    f,
-                    "Cannot create file '{}': errno {}, error: {}",
-                    filename, errno, error
-                )
-            }
-            OmFilesRsError::CannotTruncateFile {
-                filename,
-                errno,
-                error,
-            } => {
-                write!(
-                    f,
-                    "Cannot truncate file '{}': errno {}, error: {}",
-                    filename, errno, error
-                )
-            }
             OmFilesRsError::CannotOpenFileErrno { errno, error } => {
                 write!(f, "Cannot open file: errno {}, error: {}", errno, error)
-            }
-            OmFilesRsError::CannotMoveFile {
-                from,
-                to,
-                errno,
-                error,
-            } => {
-                write!(
-                    f,
-                    "Cannot move file from '{}' to '{}': errno {}, error: {}",
-                    from, to, errno, error
-                )
             }
             OmFilesRsError::ChunkHasWrongNumberOfElements => {
                 write!(f, "Chunk has wrong number of elements")
@@ -118,32 +57,11 @@ impl std::fmt::Display for OmFilesRsError {
             OmFilesRsError::DimensionMustBeLargerThan0 => {
                 write!(f, "Dimension must be larger than 0")
             }
-            OmFilesRsError::NotAOmFile => {
-                write!(f, "Not an Om file")
-            }
-            OmFilesRsError::InvalidHeaderLength => {
-                write!(f, "Invalid header length")
-            }
             OmFilesRsError::FileExistsAlready { filename } => {
                 write!(f, "File '{}' already exists", filename)
             }
-            OmFilesRsError::PosixFallocateFailed { error } => {
-                write!(f, "Posix fallocate failed: error {}", error)
-            }
-            OmFilesRsError::FtruncateFailed { error } => {
-                write!(f, "Ftruncate failed: error {}", error)
-            }
             OmFilesRsError::InvalidCompressionType => {
                 write!(f, "Invalid compression type")
-            }
-            OmFilesRsError::TryingToWriteToReadOnlyFile => {
-                write!(f, "Trying to write to read-only file")
-            }
-            OmFilesRsError::JSONSerializationError => {
-                write!(f, "JSON serialization error")
-            }
-            OmFilesRsError::UnknownVersion(v) => {
-                write!(f, "Unknown version {}", v)
             }
             OmFilesRsError::InvalidDataType => {
                 write!(f, "Invalid data type")
